fe04d37ac3dbafdc54ea85648d579882adfa6ef4
wiktor <w.sierocinski@gmail.com>
2015-09-26 17:48:48 -0700

    Refactoring sass

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -27,7 +27,6 @@ function( communicator, BaseController, UserView, User, OrganizationRepositories
     },
     _showUser: function (user) {
       var region = communicator.reqres.request('region:getRegion', 'content');
-      debugger
       var view = new UserView({model: user, collection: user.get('repositories'), region: region});
       region.show(view);
     }
